3bb2d4214be3ea313a64646689ee53be6bd3c916
warlocksoftheweb <sid.papreja@hotmail.com>
2014-01-14 09:58:20 +0530

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :description, :title
   validates_presence_of :title, :date, :description
-	validates_uniqueness_of :title
   has_many :guests, :dependent => :destroy
   end
